d7d1bbfc906104841fa7ac9734633ca945287d0b
arij <arijmahmoudi2225@gmail.com>
2024-04-23 19:46:58 +0100

    first commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  isSignDivVisiable: boolean = true; // Declare isSignDivVisiable property
+}
+
+
+
